08fa38af42a85323d687b865c2af428c74c99953
项工 <xianggong@anve.com>
2019-10-25 08:59:45 +0800

    no message

--- App/Lib/AssisitiveTouch/AppLogDetailVC.m
+++ App/Lib/AssisitiveTouch/AppLogDetailVC.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     self.navigationItem.title = self.logModel.title.ylt_isValid?self.logModel.title:self.logModel.url;
     
+    @weakify(self);
     UISearchBar *searchBar = (UISearchBar *)UISearchBar.ylt_createLayout(self.view, ^(MASConstraintMaker *make) {
         make.left.right.top.equalTo(self.view);
         make.height.mas_equalTo(44);
@@ -29,9 +30,30 @@
     searchBar.autocorrectionType = UITextAutocorrectionTypeNo;
     searchBar.autocapitalizationType = UITextAutocapitalizationTypeNone;
     
-    UITextView *textView = (UITextView *)UITextView;
+    UITextView *textView = (UITextView *)UITextView.ylt_createLayout(self.view, ^(MASConstraintMaker *make) {
+        make.left.right.bottom.equalTo(self.view);
+        make.top.equalTo(searchBar.mas_bottom);
+    });
+    textView.editable = NO;
+    textView.font = [UIFont systemFontOfSize:14];
+    NSString *contentString = [NSString stringWithFormat:@"URL:%@\n时间:%@\n\n入参:%@\n\n出参:%@", self.logModel.url, self.logModel.dateTime, self.logModel.parameters, self.logModel.result];
+    textView.text = contentString;
     
-    
+    [searchBar.searchTextField.rac_textSignal subscribeNext:^(NSString * _Nullable x) {
+        if (x.length == 0) {
+            textView.text = contentString;
+        } else {
+            NSMutableAttributedString *attributeStr = [[NSMutableAttributedString alloc] initWithString:contentString attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14]}];
+            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:x options:NSRegularExpressionCaseInsensitive |NSRegularExpressionAllowCommentsAndWhitespace error:nil];
+            NSArray *matches = [regex matchesInString:contentString options:0 range:NSMakeRange(0, contentString.length)];
+            for(NSTextCheckingResult *result in [matches objectEnumerator]) {
+                NSRange matchRange = [result range];
+                YLT_Log(@"matchRange%@",NSStringFromRange(matchRange));
+                [attributeStr setAttributes:@{NSFontAttributeName:[UIFont ylt_mediumFont:14.5],NSForegroundColorAttributeName:UIColor.redColor} range:matchRange];
+            }
+            textView.attributedText = attributeStr;
+        }
+    }];
 }
 
 - (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
